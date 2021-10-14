5986d69ec4f20d4e0b9bd003104b8189eb058851
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-10-14 11:17:35 +0530

    Update evolution_RMX1851.mk

--- evolution_RMX1851.mk
+++ evolution_RMX1851.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -18,7 +18,7 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1851
-PRODUCT_NAME := evolution_RMX1851
+PRODUCT_NAME := yaap_RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
